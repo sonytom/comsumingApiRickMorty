d385f9445df2c64131f43d232025be5b18a5cb96
tomWindwos <fichtner.dk@hotmail.com>
2022-05-19 08:54:56 -0300

    include project webflux for comsuming api rickMorty

---
+++ src/main/java/com/tomfich/comsumingapirickmorty/ComsumingApiRickMortyApplication.java
@@ -0,0 +1,13 @@
+package com.tomfich.comsumingapirickmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsumingApiRickMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComsumingApiRickMortyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomfich/comsumingapirickmorty/ComsumingApiRickMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomfich.comsumingapirickmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsumingApiRickMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

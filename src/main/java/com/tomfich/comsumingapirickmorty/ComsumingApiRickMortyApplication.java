16513b95ffa7a232fcb4fd1dc427ac4544e51645
tomWindwos <fichtner.dk@hotmail.com>
2022-05-19 09:11:00 -0300

    change name root project

--- src/main/java/com/tomfich/comsumingapirickmorty/ComsumingApiRickMortyApplication.java
+++ src/main/java/com/tomfich/comsumingapirickmorty/ComsumingApiRickMortyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComsumingApiRickMortyApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ComsumingApiRickMortyApplication.class, args);
 	}
